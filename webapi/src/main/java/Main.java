925765b1cc2c833f1494fe4ff158b111252c94ac
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-11-11 22:11:59 +0900

    Add comment

--- webapi/src/main/java/Main.java
+++ webapi/src/main/java/Main.java
@@ -2,13 +2,15 @@ import webapi.network.ApiClient;
 
 public class Main {
 
-    private static final int LOCATION_CODE_TOKYO = 130010;
+    private static final int LOCATION_CODE_TOKYO = 400040;
 
+    /**
+     * API ドキュメント
+     * http://weather.livedoor.com/weather_hacks/webservice
+     *
+     * 東京のロケーションコードは130010
+     */
     public static void main(String[] args) {
-        printWeather();
-    }
-
-    private static void printWeather() {
         ApiClient apiClient = new ApiClient();
         apiClient.getWeather(LOCATION_CODE_TOKYO,
                 (call, weather) -> System.out.println(weather.toString()));
